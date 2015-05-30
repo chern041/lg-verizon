7d9af9828838950bbfcd9b9f9e38fe16481828a6
carlosh <chern@fiu.edu>
2015-05-29 22:52:13 -0500

    Updated latest release 5/29

--- desktop/static/js/main.js
+++ desktop/static/js/main.js
@@ -1,10 +1,35 @@
-var num = 1; //number of pixels before modifying styles
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
-        $('.secondary-nav').addClass('sticky-nav');
-        $('.main-wrapper').addClass('sticky-nav');
-    } else {
-        $('.secondary-nav').removeClass('sticky-nav');
-        $('.main-wrapper').addClass('sticky-nav');
-    }
-});
+(function($) {
+	$(document).ready(function(){
+		var num = 1; //number of pixels before modifying styles
+		$(window).bind('scroll', function () {
+		    if ($(window).scrollTop() > num) {
+		        $('.secondary-nav').addClass('sticky-nav');
+		        $('.main-wrapper').addClass('sticky-nav');
+		    } else {
+		        $('.secondary-nav').removeClass('sticky-nav');
+		        $('.main-wrapper').addClass('sticky-nav');
+		    }
+		});
+
+		$('.slider-nav li').click(function(){
+			var indexSlide = $(this).index() + 1;
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			$.ajax({
+				type: 'GET',
+				url: 'static/html/sllder-content-'+indexSlide+'.html',
+				dataType: 'html',
+				success: function(html, textStatus){
+					$('.slider-content').remove();
+					$('.slider-tab').append(html);
+				},
+				error: function(xhr, textStatus, errorThrown){
+					alert('An error occurred: ' + errorThrown)
+				}
+			});
+		});
+
+
+	});
+})(jQuery);
+
--- mobile/static/js/main.js
+++ mobile/static/js/main.js
@@ -1,10 +1,18 @@
 var num = 1; //number of pixels before modifying styles
 $(window).bind('scroll', function () {
+	$(".secondary-nav ul a").css('display', 'none');
+	$(".secondary-nav ul a.active").css('display', 'block');	
     if ($(window).scrollTop() > num) {
         $('.secondary-nav').addClass('sticky-nav');
-        $('.main-wrapper').addClass('sticky-nav');
     } else {
         $('.secondary-nav').removeClass('sticky-nav');
-        $('.main-wrapper').addClass('sticky-nav');
     }
 });
+
+$(".secondary-nav ul .active").on('click', function(){
+	$(".secondary-nav ul a").toggle(0, function(){
+		$(".secondary-nav ul a.active").css('display', 'block');
+	});
+});
+
+
